80036440f06e8fc5b55b6964b42646f01ed723e8
Carmignac <carmignac.julien@gmail.com>
2020-03-23 10:52:13 +0100

    refactoriser
    Database

--- Database.php
+++ Database.php
@@ -1,30 +1,53 @@
 <?php
 
-class Database
+abstract class Database
 {
     //Nos constantes
     const DB_HOST = 'mysql:host=localhost;dbname=blog;charset=utf8';
     const DB_USER = 'root';
     const DB_PASS = 'root';
 
+    private $connection;
+
+    private function checkConnection()
+    {
+        //Vérifie si la connexion est nulle et fait appel à getConnection()
+        if($this->connection === null) {
+            return $this->getConnection();
+        }
+        //Si la connexion existe, elle est renvoyée, inutile de refaire une connexion
+        return $this->connection;
+    }
+
     //Méthode de connexion à notre base de données
-    public function getConnection()
-    
+    private function getConnection()
     {
         //Tentative de connexion à la base de données
         try{
-            $connection = new PDO(self::DB_HOST, self::DB_USER, self::DB_PASS);
-            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        //On renvoie la connexion
-        return $connection;
+            $this->connection = new PDO(self::DB_HOST, self::DB_USER, self::DB_PASS);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //On renvoie la connexion
+            return $this->connection;
         }
-        
         //On lève une erreur si la connexion échoue
         catch(Exception $errorConnection)
         {
             die ('Erreur de connection :'.$errorConnection->getMessage());
         }
+
+    }
+
+    protected function createQuery($sql, $parameters = null)
+    {
+        if($parameters)
+        {
+            $result = $this->checkConnection()->prepare($sql);
+            $result->setFetchMode(PDO::FETCH_CLASS, static::class);
+            $result->execute($parameters);
+            return $result;
+        }
+        $result = $this->checkConnection()->query($sql);
+        $result->setFetchMode(PDO::FETCH_CLASS, static::class);
+        return $result;
     }
 }
-
--- home.php
+++ home.php
@@ -1,11 +1,9 @@
 <?php
 //On inclut le fichier dont on a besoin (ici à la racine de notre site)
 require 'Database.php';
-
 //Ne pas oublier d'ajouter le fichier Article.php
 require 'Article.php';
 ?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -24,10 +22,10 @@ require 'Article.php';
     {
         ?>
         <div>
-            <h2><a href="single.php?articleId=<?= htmlspecialchars($article['id']);?>"><?= htmlspecialchars($article['title']);?></a></h2>
-            <p><?= htmlspecialchars($article['content']);?></p>
-            <p><?= htmlspecialchars($article['author']);?></p>
-            <p>Créé le : <?= htmlspecialchars($article['createdAt']);?></p>
+            <h2><a href="single.php?articleId=<?= htmlspecialchars($article->id);?>"><?= htmlspecialchars($article->title);?></a></h2>
+            <p><?= htmlspecialchars($article->content);?></p>
+            <p><?= htmlspecialchars($article->author);?></p>
+            <p>Créé le : <?= htmlspecialchars($article->createdAt);?></p>
         </div>
         <br>
         <?php
