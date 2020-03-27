92b94eaac2a142156546f8f51d7b5ee1f63bf416
Carmignac <carmignac.julien@gmail.com>
2020-03-27 14:00:37 +0100

    Model

--- src/DAO/CommentDAO.php
+++ src/DAO/CommentDAO.php
@@ -1,12 +1,31 @@
 <?php
-//Pour toutes les classes dans DAO
+
 namespace App\src\DAO;
+
+use App\src\model\Comment;
 
 class CommentDAO extends DAO
 {
+    private function buildObject($row)
+    {
+        $comment = new Comment();
+        $comment->setId($row['id']);
+        $comment->setPseudo($row['pseudo']);
+        $comment->setContent($row['content']);
+        $comment->setCreatedAt($row['createdAt']);
+        return $comment;
+    }
+
     public function getCommentsFromArticle($articleid)
     {
         $sql = 'SELECT id, pseudo, content, createdAt FROM comment WHERE article_id = ? ORDER BY createdAt DESC';
-        return $this->createQuery($sql, [$articleid]);
+        $result = $this->createQuery($sql, [$articleid]);
+        $comments = [];
+        foreach ($result as $row) {
+            $commentId = $row['id'];
+            $comments[$commentId] = $this->buildObject($row);
+        }
+        $result->closeCursor();
+        return $comments;
     }
 }
--- templates/home.php
+++ templates/home.php
@@ -1,37 +1,28 @@
-<?php
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <title>Mon blog</title>
-    </head>
+<head>
+    <meta charset="utf-8">
+    <title>Mon blog</title>
+</head>
 
-    <body>
+<body>
+<div>
+    <h1>Mon blog</h1>
+    <p>En construction</p>
+    <?php
+    foreach ($articles as $article)
+    {
+        ?>
         <div>
-            <h1>Mon blog</h1>
-            <p>En construction</p>
-            
-            <?php
-            
-            while($article = $articles->fetch())
-            {
-                ?>
-                <div>
-                <h2><a href="../public/index.php?route=article&articleid=<?= htmlspecialchars($article->id);?>"><?= htmlspecialchars($article->title);?></a></h2>
-                    <p><?= htmlspecialchars($article->content);?></p>
-                    <p><?= htmlspecialchars($article->author);?></p>
-                    <p>Créé le : <?= htmlspecialchars($article->createdAt);?></p>
-                </div>
-                <br>
-                <?php
-            }
-            $articles->closeCursor();
-            ?>
-
+            <h2><a href="../public/index.php?route=article&articleid=<?= htmlspecialchars($article->getId());?>"><?= htmlspecialchars($article->getTitle());?></a></h2>
+            <p><?= htmlspecialchars($article->getContent());?></p>
+            <p><?= htmlspecialchars($article->getAuthor());?></p>
+            <p>Créé le : <?= htmlspecialchars($article->getCreatedAt());?></p>
         </div>
-    </body>
+        <br>
+        <?php
+    }
+    ?>
+</div>
+</body>
 </html>
