64f503d69e2fd80ad7b0dcda5e055194e5c24a21
Carmignac <carmignac.julien@gmail.com>
2020-04-08 11:40:05 +0200

    Ajouter un commentaire

--- src/controller/FrontController.php
+++ src/controller/FrontController.php
@@ -2,6 +2,8 @@
 
 namespace App\src\controller;
 
+use App\config\Parameter;
+
 class FrontController extends Controller
 {
     public function home()
@@ -20,5 +22,25 @@ class FrontController extends Controller
             'article' => $article,
             'comments' => $comments
         ]);
+    }
+
+    public function addComment(Parameter $post, $articleid)
+    {
+        if($post->get('submit')) {
+            $errors = $this->validation->validate($post, 'Comment');
+            if(!$errors) {
+                $this->commentDAO->addComment($post, $articleid);
+                $this->session->set('add_comment', 'Le nouveau commentaire a bien été ajouté');
+                header('Location: ../public/index.php');
+            }
+            $article = $this->articleDAO->getArticle($articleid);
+            $comments = $this->commentDAO->getCommentsFromArticle($articleid);
+            return $this->view->render('single', [
+                'article' => $article,
+                'comments' => $comments,
+                'post' => $post,
+                'errors' => $errors
+            ]);
+        }
     }
 }
